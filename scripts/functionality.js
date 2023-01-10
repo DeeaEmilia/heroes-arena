8a821987a7e14dde295677928c2c5123f584c694
Deea Emilia Duta <deeaemiliad@gmail.com>
2023-01-10 13:28:54 +0200

    added comments

--- scripts/functionality.js
+++ scripts/functionality.js
@@ -1,37 +1,54 @@
+//declare all the dynamic(elements that will do something, like hide, when the user interacts with something else) or clickable elements you need.
+//heros is for the div that holds all 3 hero cards
 const heros = document.querySelector("#heros")
+//self explanatory
 const title = document.querySelector("#title");
+//battlelog (btn) will at first be hidden and will appear only after the fight concluded
 const battlelog = document.querySelector("#battlelog");
+//restart option = refreshes page
 const restart = document.querySelector("#restart");
+//start = shows the heros that are initially hidden and the start fight btn
 const start = document.querySelector("#start");
+//self explanatory
 const startFightBtn = document.querySelector("#start-fight");
+//this is the div that holds the battlelog and the rest of the screen blurry and darker
 const modal = document.querySelector(".modal");
+//this will generate the text for the battelog from the variable res from main.js
 const modalContainer = document.querySelector("#modal-container");
 
+
 function showHeroes() {
+    //add the d-flex class to the heros div to make the heros visible
     heros.classList.add('d-flex');
+    //add the d-none class to the title to hide it
     title.classList.add('d-none');
-  
+    //this shows the start fight btn. you can style it differently, I chose inherit because no specific styling was applied.
     startFightBtn.style.display = "inherit";
+    //add the d-none class to the start btn that hides it
     start.classList.add('d-none');
 }
 
 function roundResults() {
+    //the modal will get the innerHTML (so the text) from the variable res
     modalContainer.innerHTML = res;
+    //set to block so it displays an element as a block element. It starts on a new line, and takes up the whole width
     modal.style.display = "block";
 }
 
 function clearModal(e) {
+    //if the user clicks on the modal (so the blurry, darker background or outside the battlelog) then the battlelog closes and the restart btn appears
     if (e.target == modal) {
         modal.style.display = "none";
         restart.style.display = "inherit";
     }
 }
+
+//this just refreshes the page
 function restartGame() {
     location.reload();
 }
 
-
-
+//added event listeners for the buttons and the window itself to close the modal
 start.addEventListener('click', showHeroes);
 battlelog.addEventListener('click', roundResults);
 window.addEventListener('click', clearModal);
--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,15 @@
+//res will store our text for the battlelog 
 let res = "Battlelog: <br><br> ";
+
+//declare necessary variables for the event listener
 const selectSprite = document.querySelector("#select-sprite");
 const selectDragon = document.querySelector("#select-dragon");
 const selectDwarf = document.querySelector("#select-dwarf");
 const startFight = document.querySelector("#start-fight");
+//disabled the start fight button so that the js doesn't run until a hero is chosen
 startFight.disabled = true;
 
+//declare the Hero class with the necessary arguments and abilities
 class Hero {
     constructor(name, hp) {
         this.name = name;
@@ -15,6 +20,7 @@ class Hero {
         this.extraDamage = false;
     }
 
+    //what happens when a hero is attacked. If they possess any ability the below code will take effect, otherwise hp will directly be substracted.
     attacked(damage) {
         if (this.canFly) {
             let chance = Math.random();
@@ -42,6 +48,7 @@ class Hero {
     }
 }
 
+//use extends to create a new hero and attribute an ability to him
 class Dwarf extends Hero {
     constructor(name, hp) {
         super(name, hp);
@@ -49,9 +56,10 @@ class Dwarf extends Hero {
         this.heal = true;
     }
 
+    //define how much damage a hero does and then call the method attacked() from the parent class
     attack(otherHero) {
         let damage = 100;
-        res += "<strong>[ " + this.name + "] </strong>used <strong>[He attacc]</strong> and dealt " + damage + " damage. ";
+        res += "[ " + this.name + "] </strong>used <strong>[He attacc]</strong> and dealt " + damage + " damage. ";
         otherHero.attacked(damage);
     }
 }
@@ -70,7 +78,7 @@ class Sprite extends Hero {
             damage += 100;
             res += "The attack triggered <strong>[" + this.name + "] </strong> skill <strong>[Sass] </strong> for extra 50 damage. ";
         }
-        res += "<strong>[ " + this.name + "] </strong>used <strong>[Heart of Fire]</strong> and dealt " + damage + " damage. ";
+        res += "[ " + this.name + "] </strong>used <strong>[Heart of Fire]</strong> and dealt " + damage + " damage. ";
         otherHero.attacked(damage);
     }
 }
@@ -84,43 +92,48 @@ class Dragon extends Hero {
 
     attack(otherHero) {
         let damage = 50;
-        res += "<strong>[ " + this.name + "] </strong>used <strong style='color:#e48100;'>[Snek Attack]</strong> and dealt " + damage + " damage. ";
+        res += "[ " + this.name + "] </strong>used <strong>[Snek Attack]</strong> and dealt " + damage + " damage. ";
         otherHero.attacked(damage);
     }
 }
 
+//create a class Fight to define how the turns change and how the attack is performed
 class Fight {
     constructor(hero1, hero2) {
         this.hero1 = hero1;
         this.hero2 = hero2;
-        this.turn = 0;
+        this.turn = 1;
     }
 
     performAttack() {
-        if (this.turn === 0) {
-            res += "<strong style='color:#e72f2f;font-size:20px;'>Computer: </strong>"
+        //use %2 === 0 or not to count the turns. if the turn is even then Computer is attacking, if it's odd then You are attacking
+        if (this.turn % 2 === 0) {
+            res += "Turn " + this.turn + " <strong style='color:#e72f2f;'>Computer: </strong><strong style='color:#e72f2f;'>";
             this.hero1.attack(this.hero2);
         } else {
-            res += "<strong style='color:#61dd19;font-size:20px;'>You: </strong>"
+            res += "Turn " + this.turn + " <strong style='color:#61dd19;'>You: </strong><strong style='color:#61dd19;'>";
             this.hero2.attack(this.hero1);
         }
     }
 
     changeTurn() {
-        this.turn = 1 - this.turn;
+        //count the turns so it shows up in the battlelog
+        this.turn++;
     }
 
     findWinner() {
-        if (this.hero1.hp > 0) {
-            res += "<strong style='color:#59ba44; font-size:30px;'><br><br>" + this.hero1.name + " won with " + this.hero1.hp + " HP left. </strong>"
-        } else if (this.hero2.hp > 0) {
-            res += "<strong style='color:#59ba44;font-size:30px;'><br><br>" + this.hero2.name + " won with " + this.hero2.hp + " HP left.  </strong>"
+        //search for a winner
+        if (this.hero2.hp > 0) {
+            res += "<strong style='color:#61dd19; font-size:30px;'><br><br>" + this.hero2.name + " won with " + this.hero2.hp + " HP left. </strong>"
+        } else if (this.hero1.hp > 0) {
+            res += "<strong style='color:#61dd19;font-size:30px;'><br><br>" + this.hero1.name + " won with " + this.hero1.hp + " HP left.  </strong>"
         } else {
             res += "<strong style='color:#f29b9b;'><br><br> No heroes left alive! </strong>";
         }
     }
 
     go() {
+        //make a loop to fight while both players hp is over 0
         do {
             this.performAttack();
             this.changeTurn();
@@ -129,38 +142,49 @@ class Fight {
     }
 }
 
-
+//create 3 new objects for each hero
 let dwarf = new Dwarf("Smoliv ", 2000);
 let sprite = new Sprite("Sylveon ", 1000);
 let dragon = new Dragon("Dragonair ", 2100);
+//create an array for the computer's choice
 const array = [dwarf, sprite, dragon];
+//create 2 variables to store the player's choice and the computer's choice
 let playerChoice;
 let computerChoice;
 
+//create a function for the player's choice. The variable playerChoice will be called with an Event Listener below
 function getPlayerChoice(selection) {
     playerChoice = selection;
-    console.log(playerChoice);
+    //after the click, the start fight button will be enabled
     startFight.disabled = false;
-    // heros.style.display = "none";
 }
 
+//create a function that chooses a random index out of an array
 function getComputerChoice(arr) {
     const randomIndex = Math.floor(Math.random() * arr.length);
     const item = arr[randomIndex];
     return item;
 }
 
+//create a function that starts the fight. 
 function startFightNow() {
+    //declare a new object for the class Fight
     let epicFight = new Fight(getComputerChoice(array), playerChoice);
+    //call the method go from the class Fight
     epicFight.go();
+    //make the modal for the battlelog visible
     battlelog.style.display = "inherit";
+    //disable the start fight button so that you need to restart the game to generate a new battlelog.
     startFight.disabled = true;
 }
 
+//add event listeners one for the start fight button and 3 for the player choice.
+//in player choice, first choose the id of the btn, then add the event listener, click and then call the function with the corresponding variable for the chosen hero, as you see below:
 selectSprite.addEventListener('click', function(){getPlayerChoice(sprite);});
 selectDragon.addEventListener('click', function(){getPlayerChoice(dragon);});
 selectDwarf.addEventListener('click', function(){getPlayerChoice(dwarf);});
+//for start fight, you only need to add the startFightNow function without any arguments.
 startFight.addEventListener('click', startFightNow);
 
-
+//for other functionalities, check functionality.js file
 
